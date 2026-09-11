9e4eac3465e778d41f10d561d7e8c5f32c58accd
Prajwal Kamboj <prajwalkamboj3@gmail.com>
2026-09-11 13:09:12 +0530

    citizen registration done

--- backend/controllers/citizenController.js
+++ backend/controllers/citizenController.js
@@ -3,52 +3,51 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
 export const registerCitizen = async (req, res) => {
-   try {
-    const {name, email, password, phone} = req.body;
+    try {
+        const { name, email, password, phone } = req.body;
 
-    if (!name || !email || !password) {
-        return res.status(400).json({ success: false, message: "Please provide name, email and password" });
-    }
+        if (!name || !email || !password) {
+            return res.status(400).json({ success: false, message: "Please provide name, email and password" });
+        }
 
-    const existing = await Citizen.findOne({email})
-    if(existing)
-    {
-        return res.status(400).json({
+        const existing = await Citizen.findOne({ email })
+        if (existing) {
+            return res.status(400).json({
+                success: false,
+                message: "Citizen already exists"
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const citizen = await Citizen.create({
+            name,
+            email,
+            password: hashedPassword,
+            phone
+        })
+
+        const safeCitizen = {
+            _id: citizen._id,
+            name: citizen.name,
+            email: citizen.email,
+            phone: citizen.phone,
+            createdAt: citizen.createdAt,
+            updatedAt: citizen.updatedAt
+        }
+
+        return res.status(201).json({
+            success: true,
+            message: "Citizen registered successfully",
+            data: safeCitizen
+        })
+
+    } catch (error) {
+        res.status(500).json({
             success: false,
-            message : "Citizen already exists"
+            message: "Server Error: " + error.message
         })
     }
-
-    const hashedPassword = await bcrypt.hash(password, 10); 
-
-    const citizen = await Citizen.create({
-        name,
-        email,
-        password : hashedPassword,
-        phone
-    })
-
-    const safeCitizen = {
-        _id: citizen._id,
-        name: citizen.name,
-        email: citizen.email,
-        phone: citizen.phone,
-        createdAt: citizen.createdAt,
-        updatedAt: citizen.updatedAt
-    }
-
-    return res.status(201).json({
-        success: true,
-        message : "Citizen registered successfully",
-        data: safeCitizen
-    })
-
-   } catch (error) {
-    res.status(500).json({ 
-        success: false,
-        message : "Server Error: " + error.message
-    })
-   }
 }
 
 export const loginCitizen = async (req, res) => {
